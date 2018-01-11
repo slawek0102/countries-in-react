c2e50337d3e7fb06fb0941a008b7559e24eb705b
Slawek0102 <grzebyk.slawomir@gmail.com>
2018-01-11 19:10:16 +0100

    getDataFromCountriesApi first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
+import {InputComponent} from './Components/InputComponent/InputComponent'
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-          Hello
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <InputComponent/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/InputComponent/InputComponent.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+import './InputComponentCSS.css';
+
+export const InputComponent = (props) => (
+    <div>InputComponent</div>
+);
