c80e5b92979c70598384176c7b93435123532ff0
Slawek0102 <grzebyk.slawomir@gmail.com>
2018-02-06 11:16:28 +0100

    Some UI changes

--- src/Components/SelectedCountryDetailsComponent/SelectedCountryDetailsComponent.js
+++ src/Components/SelectedCountryDetailsComponent/SelectedCountryDetailsComponent.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import './SelectedCountryDetailsComponent.css';
-
 import Paper from 'material-ui/Paper';
 
+import './SelectedCountryDetailsComponent.css';
 
-export const SelectedCountryDetailsComponent = (props) => (
-    <div>
-
-            <Paper>
-                <p>Name: {props.details.name}</p>
-                <p>Capital: {props.details.capital}</p>
-            </Paper>
-
-            <Paper>
-                {props.details.borders.map((border) => {
-                    return (<div key={border}>Border: {border}</div>)
-                })}
-            </Paper>
-
-    </div>
-);
+const style = {
+    height: 50,
+    width: 300,
+    paddingTop: 25,
+    margin: 10,
+    textAlign: 'center',
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    lineHeight: 'normal'
+};
 
 
+export const SelectedCountryDetailsComponent = (props) => {
+    const {classes} = props;
+    return (<div>
+        <Paper>
+            <p>Name: {props.details.name}</p>
+            <p>Capital: {props.details.capital}</p>
+        </Paper>
+
+        {props.details.borders.map((border) => {
+            return (<Paper style={style} zDepth={5} key={border}>{border}</Paper>)
+        })}
+
+    </div>)
+}
 
 
