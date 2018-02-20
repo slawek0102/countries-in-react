d051a4131607beee22d082508ed6cab378869e5d
Slawek0102 <grzebyk.slawomir@gmail.com>
2018-02-20 16:00:02 +0100

    Numbers formated proparly

--- src/Components/PopulacjaSwiataComponent/PopulacjaSwiataComponent.js
+++ src/Components/PopulacjaSwiataComponent/PopulacjaSwiataComponent.js
@@ -9,9 +9,30 @@ export const PopulacjaSwiataComponent = (props) => {
 
     return (
         <div>
-            {countries.map((country)=>{
-                return (<Paper className='b-paper' zDepth={2}>{country.name} Population {country.population}</Paper>)
+            {countries.map((country) => {
+                return (<Paper className='b-paper' zDepth={3}>
+                    <div className='b-country'>{country.name}</div>
+                    <div> {formatCountryPopulation(country.population)}</div>
+                </Paper>)
             })}
         </div>
     )
+};
+
+
+let formatCountryPopulation = (population) =>{
+
+    let licznik = 1;
+
+      const reversedStringPopulationArray = population.toString().split("").reverse();
+
+       return reversedStringPopulationArray.map((digit)=>{
+           if (licznik >3){
+               licznik =2;
+               return digit + ' ';
+           } else {
+               licznik ++;
+               return digit
+           }
+      }).reverse()
 };
